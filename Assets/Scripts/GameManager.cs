56499749a1ef1e5f48867edde0b21621da8f508e
joferrer5 <joferrer@s.sfusd.edu>
2023-04-11 13:51:04 -0700

    5.1 step 6

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,22 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public GameObject[] targets2;
+    private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 
     // Update is called once per frame
