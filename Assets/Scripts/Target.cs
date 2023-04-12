a00d1a5449901752256712d1e83e33025ac318c7
joferrer5 <joferrer@s.sfusd.edu>
2023-04-12 10:56:09 -0700

    5.2 step 8

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
     private GameManager gameManager;
     public int pointValue;
     public ParticleSystem explosionParticle;
